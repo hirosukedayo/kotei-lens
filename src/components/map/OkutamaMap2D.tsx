e4ec892ba95fdeb846dd3091039c53494a53a9be
hirosukedayo <hirosuke.sz@gmail.com>
2026-02-12 08:03:22 +0900

    add-api-key

--- src/components/map/OkutamaMap2D.tsx
+++ src/components/map/OkutamaMap2D.tsx
@@ -444,7 +444,7 @@ export default function OkutamaMap2D({
         {/* ベース: Stamen Toner Lite（セピア調フィルタ適用） */}
         <MapClickHandler onClick={handleMapClick} />
         <TileLayer
-          url="https://tiles.stadiamaps.com/tiles/stamen_toner_background/{z}/{x}/{y}{r}.png"
+          url={`https://tiles.stadiamaps.com/tiles/stamen_toner_background/{z}/{x}/{y}{r}.png?api_key=${import.meta.env.VITE_STADIA_API_KEY}`}
           className="base-tiles"
           attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a> &copy; <a href="https://stamen.com/">Stamen Design</a> &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         />
