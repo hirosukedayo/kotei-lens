4403f2b317620b6c8f5ca65b34ba146839e4806a
hirosukedayo <hirosuke.sz@gmail.com>
2026-02-12 02:23:36 +0900

    revert

--- src/components/viewer/Scene3D.tsx
+++ src/components/viewer/Scene3D.tsx
@@ -37,7 +37,6 @@ import SensorPermissionRequest from '../ui/SensorPermissionRequest';
 import { getSensorManager } from '../../services/sensors/SensorManager';
 import LoadingScreen from '../ui/LoadingScreen';
 import CompassCalibration from '../ui/CompassCalibration';
-import DirectionGuide from './DirectionGuide';
 
 interface Scene3DProps {
   initialPosition?: Initial3DPosition | null;
@@ -458,9 +457,6 @@ export default function Scene3D({
 
           {/* 2Dマップ上のピン位置を3Dビューに表示 */}
           <PinMarkers3D selectedPin={selectedPin} />
-
-          {/* 方位デバッグガイド */}
-          {showDebug && <DirectionGuide />}
 
           {/* 画角(FOV)を動的に更新するコンポーネント */}
           <FovAdjuster fov={fov} />
